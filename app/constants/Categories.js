84eb36b39cdb94531e31069cb094c111aa8db1f5
Abhav <thakur.abhav25@gmail.com>
2021-08-13 13:57:14 +0530

    screens

---
+++ app/constants/Categories.js
@@ -0,0 +1,74 @@
+export const Info = [
+  {
+    img: require('../assets/DairyProduct.png'),
+    name: 'Essentials',
+    width: 40,
+    height: 40,
+  },
+  {
+    img: require('../assets/doctor.png'),
+    name: 'Doctors',
+    width: 38,
+    height: 42,
+  },
+  {
+    img: require('../assets/food.png'),
+    name: 'Restaurants',
+    width: 40,
+    height: 40,
+  },
+  {
+    img: require('../assets/shopping.png'),
+    name: 'Shopping',
+    width: 40,
+    height: 40,
+  },
+  {
+    img: require('../assets/hospital.png'),
+    name: 'Hospitals',
+    width: 38,
+    height: 42,
+  },
+  {
+    img: require('../assets/education.png'),
+    name: 'Education',
+    width: 48,
+    height: 36,
+  },
+  {
+    img: require('../assets/globe.png'),
+    name: 'Travel',
+    width: 40,
+    height: 42,
+  },
+  {
+    img: require('../assets/pills.png'),
+    name: 'Medical',
+    width: 38,
+    height: 43,
+  },
+  {
+    img: require('../assets/wedding.png'),
+    name: 'Wedding Requisites',
+    width: 40,
+    height: 40,
+  },
+  {
+    img: require('../assets/beauty.png'),
+    name: 'Saloon',
+    width: 40,
+    height: 38,
+  },
+  {
+    img: require('../assets/repair.png'),
+    name: 'Repair',
+    width: 40,
+    height: 40,
+  },
+  {
+    img: require('../assets/more.png'),
+    name: 'Show More',
+    width: 40,
+    height: 40,
+  },
+];
--- app/screens/Home/Home.js
+++ app/screens/Home/Home.js
@@ -5,12 +5,13 @@ import {
   Text,
   ScrollView,
   Dimensions,
-  Pressable,
+  FlatList,
   Image,
   TouchableOpacity,
 } from 'react-native';
 import Discount from '../../utils/Discount';
 import ImageCarousel from '../../utils/ImageCarousel';
+import {Info} from '../../constants/Categories';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -30,118 +31,27 @@ function Home(props) {
           <Text style={styles.txt}>{'\u20B9'} 2,000</Text>
         </View>
       </View>
-      <ScrollView contentContainerStyle={styles.container}>
+      <ScrollView
+        nestedScrollEnabled={true}
+        contentContainerStyle={styles.container}>
         <ImageCarousel />
         <View style={styles.category}>
-          {/* row 1 subcategory */}
-          <View
-            style={{
-              flexDirection: 'row',
-              padding: 10,
-              justifyContent: 'space-evenly',
-            }}>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/DairyProduct.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Essentials</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/doctor.png')}
-                style={{width: 38, height: 42}}
-              />
-              <Text style={styles.subtxt}>Doctors</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/food.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Restaurants</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/shopping.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Shopping</Text>
-            </TouchableOpacity>
-          </View>
-          {/* row 2 subcategory */}
-          <View
-            style={{
-              flexDirection: 'row',
-              padding: 10,
-              marginVertical: 5,
-              justifyContent: 'space-evenly',
-            }}>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/hospital.png')}
-                style={{width: 38, height: 42}}
-              />
-              <Text style={styles.subtxt}>Hospitals</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/education.png')}
-                style={{width: 48, height: 36}}
-              />
-              <Text style={styles.subtxt}>Education</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/globe.png')}
-                style={{width: 40, height: 42}}
-              />
-              <Text style={styles.subtxt}>Travel</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/pills.png')}
-                style={{width: 38, height: 43}}
-              />
-              <Text style={styles.subtxt}>Medical</Text>
-            </TouchableOpacity>
-          </View>
-          {/* row 3 subcategory*/}
-          <View
-            style={{
-              flexDirection: 'row',
-              padding: 10,
-              justifyContent: 'space-evenly',
-            }}>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/wedding.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Wedding{'\n'}Requisites</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/beauty.png')}
-                style={{width: 40, height: 38}}
-              />
-              <Text style={styles.subtxt}>Saloon</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/repair.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Repair &{'\n'}Services</Text>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
-              <Image
-                source={require('../../assets/more.png')}
-                style={{width: 40, height: 40}}
-              />
-              <Text style={styles.subtxt}>Show More</Text>
-            </TouchableOpacity>
-          </View>
+          <FlatList
+            data={Info}
+            numColumns={4}
+            nestedScrollEnabled={true}
+            horizontal={false}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) => (
+              <TouchableOpacity activeOpacity={0.4} style={styles.subcategory}>
+                <Image
+                  source={item.img}
+                  style={{width: item.width, height: item.height}}
+                />
+                <Text style={styles.subtxt}>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
         </View>
         {/* top categories */}
         <Discount />
@@ -190,8 +100,11 @@ const styles = StyleSheet.create({
     elevation: 6,
   },
   subcategory: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 1,
+    marginVertical: 10,
     alignItems: 'center',
-    marginHorizontal: 5,
   },
   subtxt: {
     color: '#293645',
